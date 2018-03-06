c306df182baa178d0887bbc4ed4c48dac098dd04
sirricdev <sirricdev@gmail.com>
2018-03-06 07:23:14 +0000

    Signup USers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
   resources :articles
   
   get 'pages/home', to: 'pages#home'
