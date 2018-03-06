6504ce49355b6d2ae63446be7d981eab98acc776
sirricdev <sirricdev@gmail.com>
2018-03-06 07:04:53 +0000

    Add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305062833) do
+ActiveRecord::Schema.define(version: 20180306070048) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180305062833) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
